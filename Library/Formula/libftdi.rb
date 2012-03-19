0322ed6bfd16e91c059699dd20206b04a93164d5
Ben Bass <benbass@codedstructure.net>
2012-03-19 21:57:00 +0000

    bump libftdi to 0.20

--- Library/Formula/libftdi.rb
+++ Library/Formula/libftdi.rb
@@ -1,9 +1,9 @@
 require 'formula'
 
 class Libftdi < Formula
-  url "http://www.intra2net.com/en/developer/libftdi/download/libftdi-0.19.tar.gz"
+  url "http://www.intra2net.com/en/developer/libftdi/download/libftdi-0.20.tar.gz"
   homepage 'http://www.intra2net.com/en/developer/libftdi'
-  md5 'e6e25f33b4327b1b7aa1156947da45f3'
+  md5 '355d4474e3faa81b485d6a604b06951f'
 
   depends_on 'boost'
   depends_on 'libusb-compat'
